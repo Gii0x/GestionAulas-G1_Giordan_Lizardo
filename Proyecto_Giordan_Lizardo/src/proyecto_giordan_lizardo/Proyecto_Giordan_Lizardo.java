bbe0e49d121b6b523adca2c784ac8176077f636a
Lizardo <lizardo.ob.2@gmail.com>
2021-03-23 18:00:49 +0100

    Prueba Lectura Fichero

--- Proyecto_Giordan_Lizardo/src/proyecto_giordan_lizardo/Proyecto_Giordan_Lizardo.java
+++ Proyecto_Giordan_Lizardo/src/proyecto_giordan_lizardo/Proyecto_Giordan_Lizardo.java
@@ -5,6 +5,9 @@
  */
 package proyecto_giordan_lizardo;
 
+import java.io.File;
+import java.util.Scanner;
+
 /**
  *
  * @author Gii0
@@ -15,7 +18,20 @@ public class Proyecto_Giordan_Lizardo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //prueba
+        File fichero = new File("/GitKrakenProject_Gio_Liz/Aula/clasroom.txt");
+        
+        try {
+            // Codificación ISO-8859-1 (ANSI) o UTF-8 dependiendo de cómo esté creado el fichero de texto
+            Scanner lectorFichero = new Scanner(fichero, "ISO-8859-1");
+            
+            while(lectorFichero.hasNext()) {
+                System.out.println(lectorFichero.nextLine());
+            }
+            
+            lectorFichero.close();
+        } catch (Exception e) {
+            System.out.println("Ha ocurrido un error al abrir/leer el fichero");
+        }
     }
     
 }
