3e250ebcf21a473ebe358478b69119c821554542
Lizardo <lizardo.ob.2@gmail.com>
2021-03-23 17:53:35 +0100

    Prueba modificación java

--- Proyecto_Giordan_Lizardo/src/proyecto_giordan_lizardo/Proyecto_Giordan_Lizardo.java
+++ Proyecto_Giordan_Lizardo/src/proyecto_giordan_lizardo/Proyecto_Giordan_Lizardo.java
@@ -15,7 +15,7 @@ public class Proyecto_Giordan_Lizardo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //prueba
     }
     
 }
