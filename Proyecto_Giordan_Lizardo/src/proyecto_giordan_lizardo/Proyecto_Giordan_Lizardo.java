b24ea0aa15e33db148b000c5297005493aa847f2
Lizardo <lizardo.ob.2@gmail.com>
2021-03-25 17:43:20 +0100

    Añadiendo funciones (Sprint 2)
    Solo queda ordenarlo con forme al proyecto

--- Proyecto_Giordan_Lizardo/src/proyecto_giordan_lizardo/Proyecto_Giordan_Lizardo.java
+++ Proyecto_Giordan_Lizardo/src/proyecto_giordan_lizardo/Proyecto_Giordan_Lizardo.java
@@ -58,3 +58,140 @@ public class Proyecto_Giordan_Lizardo {
     }
     
 }
+    public static void main(String[] args) {
+        Scanner lector=new Scanner(System.in);
+        System.out.println("Opcion a realizar como profesor");
+        int opcion=lector.nextInt();
+        switch(opcion){
+            case 1:
+                System.out.println("Opcion de Crear un fichero");
+                crearFichero();
+                break;
+            case 2:
+                System.out.println("Opcion de Añadir lineas en el fichero");
+                añadirLineasFichero();
+                break;
+            case 3:
+                System.out.println("Opcion de Eliminar lineas en el fichero");
+                eliminarLineaFichero();
+                break;
+            case 4:
+                System.out.println("Opcion de Actualizar linea en el fichero");
+                actualizarLineaFichero();
+                break;
+            default:
+        }     
+        //investigar el metodo split de la clase string
+    }
+
+    private static void crearFichero() {
+        File fichero = new File("/Test/nuevo4.txt");
+        
+        try {
+            FileWriter writer = new FileWriter(fichero);
+            
+            writer.write("Línea 1\n");
+            writer.write("Línea 2\n");
+            writer.write("Línea 3\n");
+            
+            writer.close();
+        } catch (Exception e) {
+            System.out.println("Ha ocurrido un error al crear/escribir en el fichero");
+        }
+    }
+
+    private static void añadirLineasFichero() {
+        File fichero = new File("/Test/nuevo4.txt");
+        
+        try {
+            FileWriter writer = new FileWriter(fichero,true);
+            
+            writer.write("Línea 1\n");
+            writer.write("Línea 2\n");
+            writer.write("Línea 3\n");
+            
+            writer.close();
+        } catch (Exception e) {
+            System.out.println("Ha ocurrido un error al crear/escribir en el fichero");
+        }
+    }
+
+    private static void eliminarLineaFichero() {
+        File fichero = new File("C:/Test/nuevo.txt");
+        
+        // Array para guardar todas las líneas leídas del fichero
+        ArrayList<String> lineas = new ArrayList<>();
+        
+        // Abrimos el fichero de texto para leerlo en memoria
+        try {
+            Scanner lectorFichero = new Scanner(fichero);
+            
+            int i=0;
+            
+            while(lectorFichero.hasNext()) {
+                lineas.add(lectorFichero.nextLine());
+            }
+            
+            lectorFichero.close();
+        } catch (Exception e) {
+            System.out.println("Ha ocurrido un error al abrir/leer el fichero");
+        }
+        
+        // Abrimos el fichero de texto para sobreescribirlo
+        // Eliminaremos la línea 3
+        try {
+            FileWriter writer = new FileWriter(fichero);
+            
+            for (String linea : lineas) {
+                if (!"Línea 3".equals(linea)) {
+                    writer.write(linea + "\n");
+                }
+            }
+            
+            writer.close();
+        } catch (Exception e) {
+            System.out.println("Ha ocurrido un error al abrir/sobreescribir el fichero");
+        }
+    }
+
+    private static void actualizarLineaFichero() {
+        File fichero = new File("C:/Test/nuevo.txt");
+        
+        // Array para guardar todas las líneas leídas del fichero
+        ArrayList<String> lineas = new ArrayList<>();
+        
+        // Abrimos el fichero de texto para leerlo en memoria
+        try {
+            Scanner lectorFichero = new Scanner(fichero);
+            
+            int i=0;
+            
+            while(lectorFichero.hasNext()) {
+                lineas.add(lectorFichero.nextLine());
+            }
+            
+            lectorFichero.close();
+        } catch (Exception e) {
+            System.out.println("Ha ocurrido un error al abrir/leer el fichero");
+        }
+        
+        // Abrimos el fichero de texto para sobreescribirlo
+        // Actualizaremos la línea 4
+        try {
+            FileWriter writer = new FileWriter(fichero);
+            
+            for (String linea : lineas) {
+                if ("Línea 4".equals(linea)) {
+                    writer.write("Nueva Línea 4\n");
+                } else {
+                    writer.write(linea + "\n");
+                }
+            }
+            
+            writer.close();
+        } catch (Exception e) {
+            System.out.println("Ha ocurrido un error al abrir/sobreescribir el fichero");
+        }
+    }
+    
+}
