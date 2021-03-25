2d918f200fc19a6488bb840afaca00e053ce4489
Giordan <giordan_tk@hotmail.com>
2021-03-25 15:46:21 +0100

    Modificación de código 1.0
    + Función de Matriz para leer el fichero txt y guardar datos.

--- Proyecto_Giordan_Lizardo/src/proyecto_giordan_lizardo/Proyecto_Giordan_Lizardo.java
+++ Proyecto_Giordan_Lizardo/src/proyecto_giordan_lizardo/Proyecto_Giordan_Lizardo.java
@@ -17,8 +17,11 @@ public class Proyecto_Giordan_Lizardo {
     static File fichero = new File("..\\Aulas\\clasroom.txt");
     public static void main(String[] args) {
     
-    leerFichero(fichero);   
+    String [][] clases = new String[14][7];
         
+    leerFichero(fichero);   
+    //Matriz que almacenara los datos de las clases que aparece en el fichero classroom.txt
+    rellenarMatrizClases(clases,fichero);
     }
 
     private static void leerFichero(File fichero) {
@@ -34,6 +37,12 @@ public class Proyecto_Giordan_Lizardo {
         } catch (Exception e) {
             System.out.println("Ha ocurrido un error al abrir/leer el fichero");
         }
+    }
+
+    private static void rellenarMatrizClases(String[][] clases, File fichero) {
+        
+        
+        
     }
     
 }
